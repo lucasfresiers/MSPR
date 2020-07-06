f3a9a5894458946e9b99612fe45962ad7688e278
Lucas FRESIERS <fresiers.lucas@gmail.com>
2020-07-06 09:27:48 +0200

    First commit

---
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,32 @@
+package com.example.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.uploadingfiles.storage.StorageProperties;
+import com.example.uploadingfiles.storage.StorageService;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
